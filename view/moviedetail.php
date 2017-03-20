79ac66b4b25856f7f46c3e6de54902a6fcca945e
Christian Brill <quedarme@gmail.com>
2017-03-20 16:39:43 +0100

    --

--- view/moviedetail.php
+++ view/moviedetail.php
@@ -2,7 +2,7 @@
     <div id="flexbox">
         <div id="imageSection">
             <img id="poster" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg" alt="">
-            <p id="releaseYear">Released in 1990 TODO</p>
+            <p id="releaseYear">Released: 1990 TODO</p>
             <p id="support">Support: TODO</p>
         </div>
 
@@ -10,8 +10,8 @@
             <h2>Movie Title TODO</h2>
             <h3>Category TODO</h3>
             <p id="synopsis">Synopsis TODO Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p id="actors">Actors List TODO</p>
-            <p id="path">Path of Movie TODO</p>
+            <p id="actors">Actors: TODO</p>
+            <p id="path">Path: TODO</p>
         </div>
     </div>
 </main>
