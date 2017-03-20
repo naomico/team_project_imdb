4d2e90573ce7dafebfdaa805168f2d561c83d815
Christian Brill <quedarme@gmail.com>
2017-03-20 16:15:40 +0100

    added css

--- view/header.php
+++ view/header.php
@@ -7,14 +7,14 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" href="/css/main.css">
-
-		<!-- Latest compiled and minified CSS -->
+		<!-- Latest compiled and minified CSS for Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- jQuery -->
 		<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="css/main.css">
 	</head>
 	<!-- Début du body (ce qui est "visible" sur la page) -->
 	<body>
