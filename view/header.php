fca117f15820f2e88aa3e93f46d8b8205ba9bf33
Christian Brill <quedarme@gmail.com>
2017-03-20 16:10:31 +0100

    added css and bootystrap to header

--- view/header.php
+++ view/header.php
@@ -2,9 +2,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Projet imdb</title>
+		<title>Team Projet IMDb</title>
 		<!-- Le meta charset permet de visualiser les accents, charactères spéciaux, etc. -->
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="/css/main.css">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- jQuery -->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+
 	</head>
 	<!-- Début du body (ce qui est "visible" sur la page) -->
 	<body>
