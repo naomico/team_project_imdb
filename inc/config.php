2d2067165ccf93165cdf3d5df013112c7e8536a5
Christian Brill <quedarme@gmail.com>
2017-03-20 14:42:12 +0100

    config + database files updated

---
+++ inc/config.php
@@ -0,0 +1,19 @@
+<?php
+/*---------------------------------------------
+		CONFIGURATION FILE with access
+		to the database and functions
+---------------------------------------------*/
+
+// the config file contains all the configuration values which change on different servers (dev and prod, e.g.)
+$config = array(
+		'DB_username' => 'christianb',
+		'DB_password' => 'wbforce3',
+		'DB_database' => 'christianb_sql0',
+		'DB_host' => 'wf3.progweb.fr',
+	);
+
+// inclusion of the connection to the database
+require_once dirname(__FILE__).'/db.php';
+
+// inclusion of all the functions
+require_once dirname(__FILE__).'/functions.php';
