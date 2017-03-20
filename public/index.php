0c331b0c9ea2bd286f846813aff6c29f70e4a3fd
Maxim Mehlinger <maximmehlinger@hotmail.com>
2017-03-20 15:05:18 +0100

    Merge complications SOLVED

--- public/index.php
+++ public/index.php
@@ -1,26 +1,11 @@
 <?php
-
-<<<<<<< HEAD
-// Inclusion de config.php
-// double dirname pour retrouver le répertoire parent
-require dirname(dirname(__FILE__)).'../inc/config.php';
-
-// Je récupère les données de la database dont j'ai besoin
-$sql = '
-	SELECT 
-';
-
-// FIN du code pour cette page
-
-// À la fin, je met les vues/views comme toujours
-include dirname(dirname(__FILE__)).'../view/header.php';
-include dirname(dirname(__FILE__)).'../view/home.php';
-include dirname(dirname(__FILE__)).'../view/footer.php';
-=======
 // access to config file
 require dirname(dirname(__FILE__)).'/inc/config.php';
 
-
+// Je récupère les données de la database dont j'ai besoin
+$sql = '
+	SELECT hh
+';
 
 
 
