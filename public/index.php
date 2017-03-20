17e7e8bd496ce4d3114cd39c4acab6a4b308a060
Christian Brill <quedarme@gmail.com>
2017-03-20 14:49:53 +0100

    included file access

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// access to config file
+require dirname(dirname(__FILE__)).'/inc/config.php';
+
+
+
+
+
+
+
+//=====================================
+// FILE INCLUSION
+//=====================================
+include dirname(dirname(__FILE__)).'/view/header.php';
+include dirname(dirname(__FILE__)).'/view/footer.php';
