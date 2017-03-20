7d4ef405b0ec2710916e63d2459dcc52ed003275
Christian Brill <quedarme@gmail.com>
2017-03-20 16:52:19 +0100

    worked on movie list page

---
+++ view/categories.php
@@ -0,0 +1,16 @@
+<main>
+    <div id="flexboxCat">
+        <div id="posterCat">
+
+        </div>
+
+        <div id="movieInfoCat">
+
+        </div>
+
+        <div id="buttonsCat">
+            <button type="button" name="details" id="details"><a href="#">Details</a></button>
+            <button type="button" name="edit" id="edit"><a href="#">Edit</a></button>
+        </div>
+    </div>
+</main>
