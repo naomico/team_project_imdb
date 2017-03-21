fcb4dcab64cef4f1cb8a1c8b3a37dc3edfdf6fb2
Naomi <naomicorman@gmail.com>
2017-03-21 10:45:19 +0100

    changed the database values

--- inc/config.php
+++ inc/config.php
@@ -6,9 +6,9 @@ to the database and functions
 
 // the config file contains all the configuration values which change on different servers (dev and prod, e.g.)
 $config = array(
-		'DB_username' => 'christianb',
+		'DB_username' => 'naomico',
 		'DB_password' => 'webforce3',
-		'DB_database' => 'christianb_sql0',
+		'DB_database' => 'naomico_sql0',
 		'DB_host' => 'wf3.progweb.fr',
 	);
 
