53b8950d040abbb5437613e51e10cda5ddd8e906
Naomi <naomicorman@gmail.com>
2017-03-21 14:13:07 +0100

    Add Movie Form avec les fonctions pour insérer les informations dans database (PAS DE API pour l'instant)

--- inc/functions.php
+++ inc/functions.php
@@ -16,5 +16,83 @@ function selectMovieInfo() {
     } else {
         $movieInfo = $request->fetch(PDO::FETCH_ASSOC);
     }
+}
+// function selectMovieInfo end
 
-} // function selectMovieInfo end
+function filterStringInputPost($name, $defaultValue='') {
+    $getValue = filter_input(INPUT_POST, $name);
+    if ($getValue !== false) {
+        return trim(strip_tags($getValue));
+    }
+    return $defaultValue;
+}
+function filterIntInputPost($name, $defaultValue=0) {
+    $getValue = filter_input(INPUT_POST, $name);
+    if ($getValue !== false) {
+        return intval(trim($getValue));
+    }
+    return $defaultValue;
+}
+
+function addMovie($movieTitle,$movieReleaseYear,$categoryId,$casting,$synopsis,$filePath,$supportId,$mov_poster) {
+    global $pdo;
+
+    $sql = '
+        INSERT INTO `movie`(`mov_title`,`mov_year`, `categories_cat_id`,`mov_actors`, `mov_info`, `mov_path`,`support_sup_id`,`mov_poster`)
+        VALUES (:movieTitle, :movieReleaseYear, :cat_id, :casting, :synopsis, :filePath, :sup_id, :moviePoster)
+    ';
+    $sth = $pdo->prepare($sql);
+    $sth->bindValue(':movieTitle', $movieTitle);
+    $sth->bindValue(':movieReleaseYear', $movieReleaseYear, PDO::PARAM_INT);
+    $sth->bindValue(':cat_id', $categoryId, PDO::PARAM_INT);
+    $sth->bindValue(':casting', $casting);
+    $sth->bindValue(':synopsis', $synopsis);
+    $sth->bindValue(':filePath', $filePath);
+    $sth->bindValue(':sup_id', $supportId, PDO::PARAM_INT);
+    $sth->bindValue(':moviePoster', $moviePoster);
+
+    if ($sth->execute() === false) {
+        print_r($sth->errorInfo());
+    }
+    else {
+        // Je récupère l'ID auto-incrémenté
+        return $pdo->lastInsertId();
+    }
+
+    return false;
+}
+/*
+function updateStudent($id,$lastname,$firstname,$email,$birthdate,$friendliness,$sessionId,$cityId) {
+    global $pdo;
+
+    $sql = '
+        UPDATE student
+        SET stu_lastname = :lastname,
+        stu_firstname = :firstname,
+        stu_email = :email,
+        stu_birthdate = :birthdate,
+        stu_friendliness = :friendliness,
+        session_ses_id = :ses_id,
+        city_cit_id = :cit_id
+        WHERE stu_id = :id
+    ';
+    $sth = $pdo->prepare($sql);
+    $sth->bindValue(':id', $id);
+    $sth->bindValue(':lastname', $lastname);
+    $sth->bindValue(':firstname', $firstname);
+    $sth->bindValue(':email', $email);
+    $sth->bindValue(':birthdate', $birthdate);
+    $sth->bindValue(':friendliness', $friendliness, PDO::PARAM_INT);
+    $sth->bindValue(':ses_id', $sessionId, PDO::PARAM_INT);
+    $sth->bindValue(':cit_id', $cityId, PDO::PARAM_INT);
+
+    if ($sth->execute() === false) {
+        print_r($sth->errorInfo());
+    }
+    else {
+        return true;
+    }
+
+    return false;
+}
+*/
--- view/addmovie.php
+++ view/addmovie.php
@@ -1,6 +1,13 @@
 <!-- Juste un petit test -->
 <p>Gestion du film: mov_title</p>
 <hr>
+<?php if (!empty($errorList)) : ?>
+	<div class="alert alert-danger" role="alert">
+		<?php foreach ($errorList as $currentErrorText) : ?>
+			<?= $currentErrorText ?><br>
+		<?php endforeach; ?>
+	</div>
+<?php endif; ?>
 <form action="" method="post">
 	<div class="row">
   		<div class="col-sm-3 col-sm-6 col-xs-12">
@@ -17,7 +24,7 @@
 
 			<div class="form-group">
 				<label>Catégorie</label>
-				<input type="text" class="form-control" name="cat_title" value="<?= $movieInfos['cat_title'] ?>" placeholder="Catégorie" />
+				<input type="text" class="form-control" name="cat_id" value="<?= $movieInfos['cat_id'] ?>" placeholder="Catégorie" />
 			</div>
 
 			<div class="form-group">
@@ -27,7 +34,7 @@
 
 			<div class="form-group">
 				<label>Déscription du film</label>
-				<textarea type="text" class="form-control" name="mov_synopsis" value="<?= $movieInfos['mov_info'] ?>" placeholder="Déscription du film" cols="30" rows="3"></textarea>
+				<textarea type="text" class="form-control" name="mov_info" value="<?= $movieInfos['mov_info'] ?>" placeholder="Déscription du film" cols="30" rows="3"></textarea>
 			</div>
 
 			<div class="form-group">
@@ -37,7 +44,11 @@
 
 			<div class="form-group">
 				<label>Support du fichier</label>
-				<input type="text" class="form-control" name="mov_support" value="<?= $movieInfos['mov_support'] ?>" placeholder="Support du fichier" />
+				<input type="text" class="form-control" name="sup_id" value="<?= $movieInfos['sup_id'] ?>" placeholder="Support du fichier" />
+			</div>
+			<div class="form-group">
+				<label>Affiche du film</label>
+				<input type="text" class="form-control" name="mov_poster" value="<?= $movieInfos['mov_poster'] ?>" placeholder="Affiche du film" />
 			</div>
 		</div>
 	</div>
